aaf57951e36701853c734dae467a010bee9a28d0
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2021-10-05 13:36:53 +0200

    Update ChessGame.cs

--- ChessAPI/ChessGame.cs
+++ ChessAPI/ChessGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Numerics;
+using System.Windows.Media.Imaging;
 
 namespace ChessAPI
 {
